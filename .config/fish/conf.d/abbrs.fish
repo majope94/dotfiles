d051194d3a040ace35a50f4188690da947fc6586
mjp <mp98876@googlemail.com>
2025-07-20 21:32:22 +0200

    changed fish alias to abbrs

---
+++ .config/fish/conf.d/abbrs.fish
@@ -0,0 +1,13 @@
+abbr --add ls lsd
+abbr --add l ls -l
+abbr --add la ls -a
+abbr --add lla ls -la
+abbr --add lt ls --tree
+
+abbr --add cat bat
+abbr --add d z
+abbr --add n nvim
+
+abbr --add please sudo
+
+abbr --add fp fzf --preview "bat --style=numbers --color=always --line-range :500 {}"
