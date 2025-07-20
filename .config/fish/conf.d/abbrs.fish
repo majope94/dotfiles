26cd0f46f2238b91fd8578529398d646cedb29c8
mjp <mp98876@googlemail.com>
2025-07-20 21:42:33 +0200

    changed terminal opacity and abbrs in fish

--- .config/fish/conf.d/abbrs.fish
+++ .config/fish/conf.d/abbrs.fish
@@ -7,7 +7,10 @@ abbr --add lt ls --tree
 abbr --add cat bat
 abbr --add d z
 abbr --add n nvim
+abbr --add ta tmux attach
 
 abbr --add please sudo
 
 abbr --add fp fzf --preview "bat --style=numbers --color=always --line-range :500 {}"
+
+abbr --add gco git checkout
