a06abd0d65d404cfc324acb478c326697be60c46
mjp <mp98876@googlemail.com>
2025-12-04 22:38:04 +0100

    fix new nvim plugin

--- .config/nvim/lua/custom/plugins/neocodium.lua
+++ .config/nvim/lua/custom/plugins/neocodium.lua
@@ -1,11 +1,10 @@
 -- add this to the file where you setup your other plugins:
-{
-  "monkoose/neocodeium",
-  event = "VeryLazy",
+return {
+  'monkoose/neocodeium',
+  event = 'VeryLazy',
   config = function()
-    local neocodeium = require("neocodeium")
+    local neocodeium = require 'neocodeium'
     neocodeium.setup()
-    vim.keymap.set("i", "<A-f>", neocodeium.accept)
+    vim.keymap.set('i', '<A-f>', neocodeium.accept)
   end,
 }
-
